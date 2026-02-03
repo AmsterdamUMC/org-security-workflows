78a678fd58f7361c77048a7791e3487cb8b1eca1
Bauke van der Velde <b.vandervelde@amsterdamumc.nl>
2026-02-03 10:36:42 +0100

    Rewrite to not use setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="aumc-security-hooks",
     version="1.0.0",
     description="Amsterdam UMC GitHub security pre-commit hooks",
-    package_dir={"": "pre-commit-check"},
-    py_modules=["check_filetypes", "check_personal_info"],
+    packages=["pre_commit_check"],
+    package_dir={"pre_commit_check": "pre-commit-check"},
     entry_points={
         "console_scripts": [
-            "check_filetypes=check_filetypes:main",
-            "check_personal_info=check_personal_info:main",
+            "check_filetypes=pre_commit_check.check_filetypes:main",
+            "check_personal_info=pre_commit_check.check_personal_info:main",
         ],
     },
     python_requires=">=3.8",
